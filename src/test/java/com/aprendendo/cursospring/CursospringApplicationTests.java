c82daad55d398f496e11d62266e2e0edf7fe619a
Angelo Carvalho <angelosuporteti@gmail.com>
2024-05-30 09:24:00 -0300

    Projeto criado

---
+++ src/test/java/com/aprendendo/cursospring/CursospringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aprendendo.cursospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursospringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
